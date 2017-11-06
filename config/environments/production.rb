c1a5aa3ca39b7b917d7236f47993c3b4ea07d5fd
DaveF <dfowler@rfihelper.com>
2017-11-05 19:32:16 -0500

    correct typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
    config.action_mailer.delivery_method = :smtp
    host = 'infinite-peak-60785.herokuapp.com'
    config.action_mailer.default_url_options = { host: host }
-   ActionMailer::Bae.smtp_settings = {
+   ActionMailer::Base.smtp_settings = {
      :address         => 'smtp.sendgrid.net',
      :port            => '587',
      :authentication  => plain,
