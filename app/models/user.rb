c722f7c31432b2429c9ca88c73c30c08d65a92ef
DaveF <dfowler@rfihelper.com>
2017-11-05 22:00:47 -0500

    add password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token, :activation_token       # create an accessible attribute
+  # create accessible attributes for virtual columns
+  attr_accessor :remember_token, :activation_token, :reset_token
 
   before_save   :downcase_email             # callback, like a trigger
   before_create :create_activation_digest
@@ -53,6 +54,20 @@ class User < ApplicationRecord
   # send the activation email request to the users email address
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
+  end
+
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_columns(reset_digest: User.digest(reset_token),
+                   reset_sent_at: Time.zone.now)
+  end
+
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
   end
 
   private
